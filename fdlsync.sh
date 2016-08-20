fac5da3ad7c78950f537fadfaa0727ce501f6ea9
heuristik1 <saa@ashconllc.com>
2016-08-19 23:57:46 -0700

    Update fdlsync.sh

--- fdlsync.sh
+++ fdlsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rsync -av eai@pimaster.cloudapp.net:/home/eai/master/fdl/ /usr/local/fdl > rsync-log
-cat rsync-log | mail -s "Rsync-log-$HOSTNAME" saa@ashconllc.com
+cat rsync-log | mail -s "Rsync-log-$HOSTNAME" 
 
 exit 0
